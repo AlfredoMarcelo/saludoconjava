16eebd2bc35bb37d2491ff6f743ca66bf8840c11
Alfredo Supanta <alfredo.supanta.26@gmail.com>
2022-03-08 21:38:57 -0300

    primero codigo con java, donde ingresas como argumento tu nombre y lo devuelve junto con un saludo

---
+++ Prueba.java
@@ -0,0 +1,12 @@
+package prueba;
+
+public class Prueba {
+	public static void main(String[] args) {
+		if(args.length > 0 ) {
+			System.out.println("hola "+args[0]);
+		} else {
+			System.out.println("nada de hola");			
+		}
+	}
+	
+}
